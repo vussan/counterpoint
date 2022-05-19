ef412ddfe5d362af10393f0404b56661257cc057
bhusan <bhusan@noveltytechnology.com>
2022-05-19 18:58:44 +0545

    Added hash utility

--- Business/Services/UserActivityService.cs
+++ Business/Services/UserActivityService.cs
@@ -1,6 +1,7 @@
 ﻿using Business.Abstract;
 using Repository.Core;
 using Repository.Core.Models;
+using Utils;
 
 namespace Business.Services
 {
@@ -13,6 +14,7 @@ namespace Business.Services
         }
         public async Task<UserActivity> AuthenticateUser(string username, string password)
         {
+            var hash=HashingUtility.ComputeHash("TGWA", "6E7D8C63-55E0-43B9-BCB2-6F750B8B0B95");
             return await _uow.UserActivities.Get(x => x.UaCallLetters == username && x.UaPassWord == password);
         }
     }
